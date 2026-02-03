7115c5ef9b60437cce234df3fb6daea16fc78ba4
Zenus004 <satyajyoti05@gmail.com>
2026-02-03 16:26:14 +0530

    Refactor init-pg-master.sh to create replication role and main database

--- scripts/init-pg-master.sh
+++ scripts/init-pg-master.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'password123';
-    SELECT * FROM pg_create_physical_replication_slot('replica_1_slot');
-    SELECT * FROM pg_create_physical_replication_slot('replica_2_slot');
+# Create replication user
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE ROLE $REPLICATOR_USER WITH REPLICATION LOGIN PASSWORD '$REPLICATOR_PASSWORD';
+EOSQL
+
+# Create main database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE monitoring_db;
 EOSQL
