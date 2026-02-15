b2dbd69f689a7c0501df88f13611c0db68ba939d
Zenus004 <satyajyoti05@gmail.com>
2026-02-16 00:59:59 +0530

    added API service for cluster monitoring and management

---
+++ frontend/src/services/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: '/api',
+    timeout: 15000,
+});
+
+export const getClusterStatus = async () => {
+    const response = await api.get('/monitor/status');
+    return response.data;
+};
+
+export const getAlerts = async () => {
+    const response = await api.get('/alerts');
+    return response.data;
+};
+
+export const stopContainer = async (name) => {
+    return api.post('/admin/stop', { name });
+};
+
+export const startContainer = async (name) => {
+    return api.post('/admin/start', { name });
+};
+
+export const restartContainer = async (name) => {
+    return api.post('/admin/restart', { name });
+};
+
+export default api;
